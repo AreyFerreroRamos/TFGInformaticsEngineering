2ce780f4fafe8706fd9f8d940157238f49dc56b4
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-10-17 17:50:47 +0200

    Arey: Inici de la traducció del projecte per fer un assaig de nestedness d'un conjunt de matrius de python a c.

--- c/nestedness_assesment.c
+++ c/nestedness_assesment.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    double nested_value;
+    double p_value;
+} nested_components;
+
+int abundances_individuals_matrix()
+{
+    return 0;
+}
+
+int abundances_vertebrates_matrix()
+{
+    return 0;
+}
+
+void discretize_matrix()
+{
+
+}
+
+void nestedness_assesment()
+{
+
+}
 
 int main(int argc, char * argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+    double **abundances_matrix;
+
+    if (! strcmp(argv[4], "individuals"))
+    {
+        abundances_matrix = abundances_individuals_matrix();
+    }
+    else if (! strcmp(argv[4], "vertebrates"))
+    {
+        abundances_matrix = abundances_vertebrates_matrix()
+    }
+
+    discretize_matrix(abundances_matrix);
+    nestedness_assesment(abundances_matrix, atoi(argv[5]))
+    printf("\nnested value: %.2f\np-value: %.2f", nested_components.nested_value, nested_components.p_value)
 }
